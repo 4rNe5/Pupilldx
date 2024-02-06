d7573bb232e2dc4e31867df71549a8088169c6ae
박준현 <me@4rne5.dev>
2024-02-07 06:04:28 +0900

    Edit ScreenShotAvailable

--- app/src/main/java/xyz/quaver/pupil/ui/BaseActivity.kt
+++ app/src/main/java/xyz/quaver/pupil/ui/BaseActivity.kt
@@ -54,10 +54,6 @@ open class BaseActivity : AppCompatActivity() {
         super.onResume()
 
         if (Preferences["security_mode"])
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE)
-        else
             window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
 
         if (locked)
